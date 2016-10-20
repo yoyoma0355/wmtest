9fb33451ef17aff54ba08e325b985cb3d2eb7532
Administrator <331992821@qq.com>
2016-10-20 14:30:44 +0800

    1-9全排序 362880种

--- test.py
+++ test.py
@@ -1,55 +1,67 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 '''
-func1()   #这个会失败的原因是，func1的def代码还未执行
+题目28	(本题无答案)：1-9全排列
+写一个程序，输出1-9的所有全排列。
 
-def func1():
-    print(func2())
+本文实例讲述了常规方法实现python数组的全排列操作。分享给大家供大家参考。具体分析如下：
 
-func2()   #这里失败原因同上，所在def在前，调用在后。
-def func2():
-    return "hello"
-
-func1()
-func2()
+全排列解释：从n个不同元素中任取m（m≤n）个元素，按照一定的顺序排列起来，叫做从n个不同元素中取出m个元素的一个排列。当m=n时所有的排列情况叫全排列。
 
 
-class rec:
-    pass
+def perm(l):
+  if(len(l)<=1):
+    return [l]
+  r=[]
+  for i in range(len(l)):
+    s=l[:i]+l[i+1:]
+    p=perm(s)
+    for x in p:
+      r.append(l[i:i+1]+x)
+  return r
 
-rec.name='wangmin'
-rec.age='34'
-rec.sex='woman'
-rec.job='it'
+调用方法：
+if __name__=='__main__':
+  """ default param is list(1,2,3,4,5) """
+  l=[];
+  if(len(sys.argv)<=1):
+    """input=['%d' %(i) for i in xrange(1,6)]"""
+    l=list((1,2,3,4,5))
+  else:#input param looks like "2,3,4,5,6",no legal checks here.
+    input=str(sys.argv[1])
+    l=input.split(",")
+    for i in xrange(len(l)):
+      l[i] = int(l[i])
+  print perm(l)
 
-print rec.age
+希望本文所述对大家的Python程序设计有所帮助。
+
 '''
-#
-#pickup_day =(1,2)
-#
-#def get_max_pickup_day(self):
-#    for x in self.pickup_day:
-#        re= self.get_api(0)
-#        if re==():
-#            return x
-#            break
-#        continue
-#    return x
-
-
-#
-#
-#
-#
-#file = open(r'E:\3--rms_test_api\data\20160927141851162_1_mstbillrev.json','r')
-#for (num,value) in enumerate(file):
-#    print "line:",num,"is:",value
-#file.close()
 
 
 
 
-
-
+if len(sys.argv) < 2:
+    print 'No action specified.'
+    sys.exit()
+if sys.argv[1].startswith('--'):
+    option = sys.argv[1][2:]
+    # fetch sys.argv[1] but without the first two characters
+    if option == 'version':  #当命令行参数为-- version，显示版本号
+        print 'Version 1.2'
+    elif option == 'help':  #当命令行参数为--help时，显示相关帮助内容
+        print '''\
+This program prints files to the standard output.
+Any number of files can be specified.
+Options include:
+  --version : Prints the version number
+  --help    : Display this help'''
+    else:
+        print 'Unknown option.'
+    sys.exit()
+else:
+    for filename in sys.argv[1:]: #当参数为文件名时，传入readfile，读出其内容
+        readfile(filename)
 
---
+++ test_4_20161019.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+题目28	(本题无答案)：1-9全排列
+写一个程序，输出1-9的所有全排列。
+
+本文实例讲述了常规方法实现python数组的全排列操作。分享给大家供大家参考。具体分析如下：
+
+全排列解释：从n个不同元素中任取m（m≤n）个元素，按照一定的顺序排列起来，叫做从n个不同元素中取出m个元素的一个排列。当m=n时所有的排列情况叫全排列。
+
+'''
+
+import sys
+
+def perm(l):
+  if(len(l)<=1):
+    return [l]
+  r=[]
+  for i in range(len(l)):
+    s=l[:i]+l[i+1:]
+    p=perm(s)    #递归调用，两个return完了都到这里，递归的出口是什么我还没有理清楚。
+    for x in p:
+      r.append(l[i:i+1]+x)
+  return r
+
+
+if __name__=='__main__':
+  """ default param is list(1,2,3,4,5) """
+  l=[];
+  if(len(sys.argv)<=1):
+    """input=['%d' %(i) for i in xrange(1,6)]"""
+    l=list((1,2,3,4,5,6,7,8,9))
+  else:#input param looks like "2,3,4,5,6",no legal checks here.
+    input=str(sys.argv[1])    #接收命令行参数
+    l=input.split(",")
+    for i in xrange(len(l)):
+      l[i] = int(l[i])
+  res=perm(l)
+  print len(res)  #g1-9的全排列的个数为362880种
+  #print perm(l)
+
